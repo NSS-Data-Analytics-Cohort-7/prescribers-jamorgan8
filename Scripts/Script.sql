87233d453420e5cb000539e736b0e257d6122d4f
Jacob Morgan <jamorgan8@gmail.com>
2022-08-11 21:45:36 -0500

    test push 2. Trust, but verify

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -101,3 +101,5 @@ LIMIT 5;
 /* 4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.*/
 
 /*    b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision. */
+
+
