e2b6e861777f16b484c687dc6661b904d573fa53
Jacob Morgan <jamorgan8@gmail.com>
2022-08-13 11:39:21 -0500

    working on 6c. My internet is chugging....

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -43,7 +43,7 @@ ORDER BY total_claims DESC;
 --   b. Which specialty had the most total number of claims for opioids?
 
 SELECT specialty_description AS specialty, 
-       SUM(p2.total_claim_count) AS total_claims
+       COUNT(p2.total_claim_count) AS total_claims
 FROM prescriber AS p1
 LEFT JOIN prescription AS p2
     ON p1.npi = p2.npi
@@ -55,9 +55,7 @@ WHERE total_claim_count IS NOT NULL
 GROUP BY specialty
 ORDER BY total_claims DESC;
 
---ANSWER-- Nurse Practitioner with 90,0845
-
-
+--ANSWER-- Nurse Practitioner with 9,551
 
 
 --   c. Challenge Question: Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
@@ -70,7 +68,7 @@ ORDER BY total_claims DESC;
 --3. a. Which drug (generic_name) had the highest total drug cost?
 
 SELECT d1.generic_name AS name,
-    ROUND(MAX(p1.total_drug_cost), 2) AS cost
+    ROUND(SUM(p1.total_drug_cost), 2) AS cost
 FROM drug AS d1
 LEFT JOIN prescription AS p1
     ON d1.drug_name = p1.drug_name
@@ -86,7 +84,7 @@ LIMIT 5;
 --   b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
 
 SELECT d1.generic_name AS name,
-    ROUND(MAX(p1.total_day_supply), 2) AS daily_cost
+    ROUND(SUM(p1.total_drug_cost) / 365, 2) AS daily_cost
 FROM drug AS d1
 LEFT JOIN prescription AS p1
     ON d1.drug_name = p1.drug_name
@@ -95,11 +93,70 @@ GROUP BY generic_name
 ORDER BY daily_cost DESC
 LIMIT 5;
 
---ANSWER-- Levothyroxine Sodium costs $115,546.00
+--ANSWER-- INSULIN GLARGINE,HUM.REC.ANLOG costs $285,654.98
 
 
 /* 4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.*/
 
-/*    b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision. */
+SELECT d.drug_name,
+	   CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+	        WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+		    ELSE 'neither'
+			END AS drug_type
+FROM drug AS d
+GROUP BY d.drug_name, drug_type;
 
+
+
+/* b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision. */
+
+SELECT d1.drug_name,
+	   p1.total_drug_cost AS drug_cost,
+	   CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+	        WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+		    ELSE 'neither'
+			END AS drug_type
+FROM drug AS d1
+LEFT JOIN prescription AS p1
+	ON d1.drug_name = p1.drug_name
+GROUP BY d1.drug_name, drug_type, drug_cost
+ORDER BY drug_type;
+
+
+
+/*5. a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee. */
+
+SELECT COUNT(cbsaname) 
+FROM cbsa
+WHERE cbsaname LIKE '%TN';
+
+--ANSWER-- 33 in TN
+
+/* b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.*/
+
+SELECT cbsa,
+	   MAX(p1.population) AS population
+FROM cbsa
+LEFT JOIN population as p1
+	ON cbsa.fipscounty = p1.fipscounty
+WHERE population IS NOT NULL 
+GROUP BY cbsa
+ORDER BY population DESC;
+
+SELECT cbsa,
+	   MIN(p1.population) AS population
+FROM cbsa
+LEFT JOIN population as p1
+	ON cbsa.fipscounty = p1.fipscounty
+WHERE population IS NOT NULL 
+GROUP BY cbsa
+ORDER BY population;
+
+--ANSWER-- 32820 has the most @ 937847. 16860 has the least @ 14654
+
+
+
+ 
+
+/* c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.*/
 
